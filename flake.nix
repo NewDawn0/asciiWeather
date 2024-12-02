2d925fdc70863353f5dc9ffd8a317964d204f4b0
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-02 23:48:29 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "An ascii weather screensaver";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix-systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { nixpkgs, ... }@inputs:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
+      mkPkgs = system: nixpkgs.legacyPackages.${system};
+    in {
+      packages = eachSystem (system:
+        let pkgs = mkPkgs system;
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            name = "ascii-weather";
+            version = "1.0.0";
+            src = ./.;
+            buildInputs = with pkgs; [ gnumake gcc ncurses ];
+            buildPhase = "make build";
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ascii-weather $out/bin
+            '';
+          };
+        });
+    };
+}
