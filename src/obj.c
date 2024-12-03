ae74c37cca6f85282dbdd4df76c752c42300b828
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-03 02:48:43 +0100

    Add object

--- src/obj.c
+++ src/obj.c
@@ -2,9 +2,10 @@
 #include "config.h"
 #include "util.h"
 #include <curses.h>
+#include <stdbool.h>
 
 void objShow(Obj *self);
-void objShift(Obj *self);
+void objShift(Obj *self, bool rand);
 
 const char *RAIN_CHARS;
 const short *RAIN_COLS;
@@ -16,31 +17,53 @@ void initExternCfg() {
 }
 
 Obj newObj() {
+  int typeRepr = randRange(1, 3); // Set type representation
   Obj out = {
       .x = randRange(0, getmaxx(stdscr) - 1),
-      .y = 0,
-      .origX = 0,
-      .origY = 0,
+      .y = randRange(0, getmaxy(stdscr) - 1),
       .forceX = 0, // No x force
-      .forceY = 1, // Default falling down
-      .repr = 'a',
       .show = objShow,
       .shift = objShift,
   };
+  switch (weatherType) {
+  case Rain:
+    out.repr = (typeRepr < 2) ? RAIN_CHARS[0] : RAIN_CHARS[1];
+    out.col = (randRange(1, 4) < 2) ? 1 : 2;
+    // Different speeds for different drops
+    if (typeRepr < 2) {
+      out.forceY = 1;
+    } else {
+      out.forceY = randRange(1, 3);
+    }
+    break;
+  }
+  out.origX = out.x;
+  out.origY = 0;
   return out;
 }
 
-void objShow(Obj *self) { mvaddch(self->y, self->x, self->repr); }
+void objShow(Obj *self) {
+  attron(COLOR_PAIR(self->col));
+  mvaddch(self->y, self->x, self->repr);
+}
 
-void objShift(Obj *self) {
+void objShift(Obj *self, bool rand) {
   self->x += self->forceX;
   self->y += self->forceY;
   // Bottom & top
   if (self->y >= getmaxy(stdscr) || self->y <= -1) {
     self->y = self->origY;
+    if (rand)
+      self->x = randRange(0, getmaxx(stdscr) - 1);
+    if (!rand)
+      self->x = self->origX;
   }
   // Right & left
   if (self->x >= getmaxx(stdscr) || self->x <= -1) {
     self->y = self->origY;
+    if (rand)
+      self->x = randRange(0, getmaxx(stdscr) - 1);
+    if (!rand)
+      self->x = self->origX;
   }
 }
--- src/obj.h
+++ src/obj.h
@@ -1,11 +1,14 @@
 #ifndef OBJ_H
 #define OBJ_H
 
+#include <stdbool.h>
+
 typedef struct obj_t {
   int x, y, origX, origY, forceX, forceY;
+  short col;
   char repr;
   void (*show)(struct obj_t *);
-  void (*shift)(struct obj_t *);
+  void (*shift)(struct obj_t *, bool);
 } Obj;
 
 Obj newObj();
