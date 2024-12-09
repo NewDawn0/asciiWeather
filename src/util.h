0c16fa90519156eb797e425b9b42fa3c3c421bb6
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-10 00:49:04 +0100

    Use drawfns

--- src/util.h
+++ src/util.h
@@ -7,6 +7,22 @@
 
 #define len(array) (sizeof(array) / sizeof((array)[0]))
 
+struct draw_obj_t {
+  short col;
+  int x, y;
+  char repr;
+};
+typedef struct draw_container_t {
+  size_t idx;
+  struct draw_obj_t objs[4096];
+  void (*show)(struct draw_container_t *);
+  void (*exit)(struct draw_container_t *);
+  void (*add)(struct draw_container_t *, short col, int x, int y, char repr);
+  void (*clean)(struct draw_container_t *);
+} DrawContainer;
+
+DrawContainer *newDrawContainer(); // initial size
+
 int randRange(int min, int max);
 int *randSel(int *arr, size_t size);
 
