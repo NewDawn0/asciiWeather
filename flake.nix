09bd12f54ca0391196aaff1bce993e83b983538b
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 12:44:37 +0100

    update flake using nixUtils

--- flake.nix
+++ flake.nix
@@ -1,38 +1,32 @@
 {
   description = "An ascii weather screensaver";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix-systems.url = "github:nix-systems/default";
-  };
+  inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { nixpkgs, ... }@inputs:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
-      mkPkgs = system: nixpkgs.legacyPackages.${system};
-    in {
-      packages = eachSystem (system:
-        let pkgs = mkPkgs system;
-        in {
-          default = pkgs.gccStdenv.mkDerivation {
-            pname = "ascii-weather";
-            version = "1.0.0";
-            src = ./.;
-            buildInputs = with pkgs; [ ncurses ];
-            installPhase = "install -D ascii-weather -t $out/bin";
-            meta = {
-              description =
-                "An ASCII-based screensaver that shows various weather conditions";
-              longDescription = ''
-                This screensaver uses ASCII art to display different weather types.
-                It's a creative and simple way to keep your terminal lively while providing weather updates.
-              '';
-              homepage = "https://github.com/NewDawn0/asciiWeather";
-              license = pkgs.lib.licenses.mit;
-              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-              platforms = pkgs.lib.platforms.all;
-            };
-          };
-        });
+  outputs = { self, utils, ... }: {
+    overlays.default = final: prev: {
+      ascii-weather = self.packages.${prev.system}.default;
     };
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = pkgs.gccStdenv.mkDerivation {
+        pname = "ascii-weather";
+        version = "1.0.0";
+        src = ./.;
+        buildInputs = with pkgs; [ ncurses ];
+        installPhase = "install -D ascii-weather -t $out/bin";
+        meta = {
+          description =
+            "ASCII-based screensaver that shows various weather conditions";
+          longDescription = ''
+            This screensaver uses ASCII art to display different weather types.
+            It's a creative and simple way to keep your terminal lively while providing weather updates.
+          '';
+          homepage = "https://github.com/NewDawn0/asciiWeather";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platforms = pkgs.lib.platforms.all;
+        };
+      };
+    });
+  };
 }
