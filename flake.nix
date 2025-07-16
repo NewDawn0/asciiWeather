9a4af9b618c2aaeeff9185b6042f8779a14663c5
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-07-16 19:41:20 +0200

    add nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
   description = "An ascii weather screensaver";
 
-  inputs.utils.url = "github:NewDawn0/nixUtils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils = {
+      url = "github:NewDawn0/nixUtils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = { self, utils, ... }: {
     overlays.default = final: prev: {
